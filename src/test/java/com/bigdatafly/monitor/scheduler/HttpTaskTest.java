eda32eea899adae1ab795ba2270194bd4e4b00e4
xihua.zhu <xihua.zhu@bwcmall.com>
2016-05-29 20:44:01 +0800

    c

---
+++ src/test/java/com/bigdatafly/monitor/scheduler/HttpTaskTest.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.bigdatafly.monitor.scheduler;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import com.bigdatafly.monitor.http.HbaseJmxQuery;
+
+/**
+ * @author summer
+ *
+ */
+public class HttpTaskTest {
+
+	SchedulerManager schedulerManager = new SchedulerManager();
+	HbaseJmxQuery hbaseJmxQuery = schedulerManager.getHbaseJmxQuery();
+	private static final ExecutorService executor = Executors.newCachedThreadPool();
+	
+	private Task createMasterTask(String masterJmxQuery){
+		return schedulerManager.createMasterTask(masterJmxQuery);
+	}
+	
+	@Test
+	public void masterAndRegionServerTaskTest() throws InterruptedException{
+		
+		String masterJmxQuery = hbaseJmxQuery.getMasterJmxQuery();
+		Task masterTask = createMasterTask(masterJmxQuery);
+		String regionServerJmxQuery = this.hbaseJmxQuery.getRegionServerJmxQuery();
+		Task regionServer = schedulerManager.createRegionServerTask(regionServerJmxQuery);
+		executor.execute(masterTask);
+		executor.execute(regionServer);
+		
+		
+		executor.awaitTermination(10, TimeUnit.MINUTES);
+	}
+	
+}
--- src/test/java/com/bigdatafly/monitor/scheduler/SchedulerManagerTest.java
+++ src/test/java/com/bigdatafly/monitor/scheduler/SchedulerManagerTest.java
@@ -3,13 +3,9 @@
  */
 package com.bigdatafly.monitor.scheduler;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 import org.junit.Test;
 
-import com.bigdatafly.monitor.http.HbaseJmxQuery;
+import com.bigdatafly.monitor.exception.HbaseMonitorException;
 
 /**
  * @author summer
@@ -17,26 +13,11 @@ import com.bigdatafly.monitor.http.HbaseJmxQuery;
  */
 public class SchedulerManagerTest {
 
-	SchedulerManager schedulerManager = new SchedulerManager();
-	HbaseJmxQuery hbaseJmxQuery = schedulerManager.getHbaseJmxQuery();
-	private static final ExecutorService executor = Executors.newCachedThreadPool();
-	
-	private Task createMasterTask(String masterJmxQuery){
-		return schedulerManager.createMasterTask(masterJmxQuery);
-	}
-	
 	@Test
-	public void masterAndRegionServerTaskTest() throws InterruptedException{
+	public void schedulerManagerTest() throws HbaseMonitorException{
 		
-		String masterJmxQuery = hbaseJmxQuery.getMasterJmxQuery();
-		Task masterTask = createMasterTask(masterJmxQuery);
-		String regionServerJmxQuery = this.hbaseJmxQuery.getRegionServerJmxQuery();
-		Task regionServer = schedulerManager.createRegionServerTask(regionServerJmxQuery);
-		executor.execute(masterTask);
-		executor.execute(regionServer);
-		
-		
-		executor.awaitTermination(10, TimeUnit.MINUTES);
+		SchedulerManager schedulerManager = new SchedulerManager();
+		schedulerManager.start();
+		schedulerManager.await();
 	}
-	
 }
