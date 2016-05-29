58160d6764eb83131802dae73c18aec40743bfdd
xihua.zhu <xihua.zhu@bwcmall.com>
2016-05-30 00:50:33 +0800

    spring-test

---
+++ src/test/java/com/bigdatafly/hbase/spring/HbaseTemplateTest.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.bigdatafly.hbase.spring;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author summer
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+public class HbaseTemplateTest {
+
+}
