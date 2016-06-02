b7f5e3dea7923dc88d5572a25c39525fe96475f6
summer <summer@summer-PC>
2016-06-02 17:33:55 +0800

    c

---
+++ src/main/java/com/bigdatafly/monitor/messages/ProtocolConstants.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.bigdatafly.monitor.messages;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author summer
+ *
+ */
+public class ProtocolConstants {
+
+	/*
+	 * 协议格式
+	 * 
+	 * 协议头
+	 *PROTOCOL_HEADER_MODEL_NAME   		工程模块 
+	 * PROTOCOL_HEADER_SERVER_NODE_NAME 服务器节点名称
+	 * PROTOCOL_HEADER_TIME_STAMP		ts
+	 * 
+	 * 内容
+	 * body
+	 */
+	
+
+	//public static final String PROTOCOL_HEADER_PROJECT_NAME="projectName"; //目前就2种
+	public static final String PROTOCOL_HEADER_MODEL_NAME="modeName";
+	public static final String PROTOCOL_HEADER_SERVER_NODE_NAME = "serverNodeName";
+	public static final String PROTOCOL_HEADER_TIME_STAMP = "timeStamp";
+	public static final String PROTOCOL_HEADER_RESOURCE = "urlresource";
+	
+	public static final String PROTOCOL_HEADER_MODEL_UNKNOWN_CODE = "0";
+	public static final String PROTOCOL_HEADER_MODEL_HBASE_CODE = "1";
+	public static final String PROTOCOL_HEADER_MODEL_STORM_CODE = "2";
+	
+	public static final String PROTOCOL_HEADER_MODEL_HBASE="hbase";
+	public static final String PROTOCOL_HEADER_MODEL_STORM="storm";
+	
+	public static final String MASTER_MONITOR_TYPE="1";
+	public static final String REGION_SERVER_MONITOR_TYPE="2";
+	public static final String REGION_MONITOR_TYPE="3";
+	public static final String JVM_MONITOR_TYPE="4";
+	
+	public static final String getModelCode(String model){
+		
+		String modelCode = PROTOCOL_HEADER_MODEL_UNKNOWN_CODE;
+		if(!StringUtils.isEmpty(model)){
+			if(PROTOCOL_HEADER_MODEL_HBASE.equals(model))
+				modelCode = PROTOCOL_HEADER_MODEL_HBASE_CODE;
+			if(PROTOCOL_HEADER_MODEL_STORM.equals(model))
+				modelCode = PROTOCOL_HEADER_MODEL_STORM_CODE;
+		}
+		return modelCode;	
+	}
+	
+}
---
+++ src/main/java/com/bigdatafly/monitor/scheduler/AbstractConsumer.java
@@ -0,0 +1,114 @@
+/**
+ * 
+ */
+package com.bigdatafly.monitor.scheduler;
+
+import java.util.Deque;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bigdatafly.monitor.exception.HbaseMonitorException;
+import com.bigdatafly.monitor.messages.Message;
+import com.bigdatafly.monitor.mq.MessageQueue;
+import com.google.common.collect.Lists;
+
+/**
+ * @author summer
+ *
+ */
+public abstract class AbstractConsumer implements Consumer{
+
+	private static final Logger logger = LoggerFactory.getLogger(AbstractConsumer.class);
+	
+	protected Thread innerThread;
+	protected volatile boolean stop;
+	//private MonitorDataOperator hbaseOperator;
+	//private Map<String,Message> mq;
+	protected boolean failDiscard = true;
+	//private int batchSize;  不能入库
+	//private static final int DEFAULT_BATCH_SIZE = 1;
+	
+	public AbstractConsumer(){
+		this(true);	
+	}
+	
+	public AbstractConsumer(boolean failDiscard){
+		
+		this.stop = false;
+		this.failDiscard = failDiscard;
+		//this.mq = mq;	
+	}
+	
+	public void start(){
+		innerThread = new Thread(new Runnable(){
+			@Override
+			public void run() {
+				while(true){
+					if(stop || innerThread.isInterrupted())
+						break;
+					if(!MessageQueue.isEmpty()){
+						synchronized(MessageQueue.mq){
+							Deque<Message> discardMessages = Lists.newLinkedList();
+							while(true){
+								Message value = MessageQueue.poll();
+								if(value == null)
+									break;
+								else{
+									try {
+										if(logger.isDebugEnabled())
+											logger.debug(value.toString());
+										consumer(value);
+									} catch (HbaseMonitorException e) {
+										discardMessages.addFirst(value);
+									}
+								}
+							
+							}//while
+						
+							if(!failDiscard){
+								MessageQueue.mq.addAll(discardMessages);
+							}
+						}
+					}
+					try{
+						Thread.sleep(200);
+					}catch(InterruptedException ex){
+						stop = true;
+						break;
+					}
+				}
+			}
+		});
+		
+		innerThread.setDaemon(true);
+		innerThread.start();
+	}
+	
+	public void stop(){
+			
+		stop = true;
+		innerThread.interrupt();
+	}
+	
+	
+	public boolean isStop() {
+		return stop;
+	}
+
+	public void setStop(boolean stop) {
+		this.stop = stop;
+	}
+
+	public boolean isFailDiscard() {
+		return failDiscard;
+	}
+
+	public void setFailDiscard(boolean failDiscard) {
+		this.failDiscard = failDiscard;
+	}
+
+	public abstract void consumer(Message msg) throws HbaseMonitorException;
+	
+	
+}
